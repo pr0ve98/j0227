f33fb11902108b95e092c4fb3044fd519eaac51e
pr0ve98 <mq5abe@naver.com>
2024-02-28 11:23:20 +0900

    20240228_1123 제어문 연습

---
+++ src/t3_제어문/Test6.java
@@ -0,0 +1,26 @@
+package t3_제어문;
+
+import java.util.Scanner;
+
+public class Test6 {
+	public static void main(String[] args) {
+		
+		// 입력받은 수가 양수, 음수, 0인지 판별하시오.
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int su = 0;
+		String str = "";
+		
+		System.out.print("숫자를 입력하세요. ");
+		su = sc.nextInt();
+		
+		if(su > 0) str = "양수";
+		else if(su < 0) str = "음수";
+		else str = "0";
+		
+		System.out.println("입력된 수 "+su+"는 "+str+"입니다.");
+		
+		sc.close();
+	}
+}
